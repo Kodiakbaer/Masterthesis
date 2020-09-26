7355e0a89f1e7e71ff15b20804bc4c4a29e4e94c
Kodiakbaer <m.michen@hotmail.com>
2020-09-26 15:59:42 +0200

    Commit of Project1.py

--- Pythonworkspace/Project2.py
+++ Pythonworkspace/Project2.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 #############################
-widthImg = 640
-heightImg = 480
+widthImg = 480
+heightImg = 640
 
 #############################
 
@@ -47,16 +47,36 @@ def getContours(img):
 
 
 def reorder (myPoints):
-    pass
 
+    # if myPoints.size  == 0:
+    #     myPoints = np.zeros((4, 1, 2), np.int32)
+
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), np.int32)
+    add =myPoints.sum(1)
+
+    #print("add", add)
+
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    #print("New Points", myPointsNew)
+
+    return myPointsNew
 
 def getWarp(img, biggest):
-
+    biggest = reorder(biggest)
+    print(biggest)
     pts1 = np.float32(biggest)
     pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
     imgOut = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
 
+    imgCropped = imgOut[20:imgOut.shape[0]-20, 20:imgOut.shape[1]-20]
+    imgCropped = cv2.resize(imgCropped, (widthImg, heightImg))
     return imgOut
 
 
@@ -67,9 +87,12 @@ while True:
 
     imgThres = preProcessing(img)
     biggest = getContours(imgThres)
-    print(biggest)
-    imgWarp = getWarp(img, biggest)
+    # print(biggest)
+    if biggest.size != 0:
+        imgWarp = getWarp(img, biggest)
+
 
     cv2.imshow("Result", imgWarp)
+    cv2.imshow("CameraFeed", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
