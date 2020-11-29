c0b2e22de5e93f6cacab755b5fd80fe4c184afb6
Kodiakbaer <m.michen@hotmail.com>
2020-11-29 20:57:30 +0100

    Extraction of Holds Testcode created

--- Pythonworkspace/OpenCVChapter5.py
+++ Pythonworkspace/OpenCVChapter5.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("Resources/Cards.jpeg")
+img = cv2.imread("Resources/LSW.jpg")
 
-width, height = 900, 1490
-pts1 = np.float32([[345,370],[872,366],[208,1321],[968,1330]])
+width, height = 2000, 2000
+pts1 = np.float32([[710,240],[3270,290],[620,2820],[3295,2800]])
 pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
 matrix = cv2.getPerspectiveTransform(pts1,pts2)
 imgOut = cv2.warpPerspective(img, matrix, (width,height))
@@ -13,4 +13,5 @@ imgOut = cv2.warpPerspective(img, matrix, (width,height))
 cv2.imshow("Image", img)
 cv2.imshow("Output", imgOut)
 
+cv2.imwrite("LSW.jpg", imgOut)
 cv2.waitKey(0)
